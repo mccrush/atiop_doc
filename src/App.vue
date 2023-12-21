9cbc66d2b0f85fa5f110bb79ee958a61b4addeec
mccrush <mccrush2027@gmail.com>
2023-12-21 17:31:13 +0700

    Create first view

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <TheNavbar />
+    <div class="container-fluid pt-3 pb-5">
+      <component :is="component" />
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheNavbar from './components/interface/TheNavbar.vue'
+import TheFooter from './components/interface/TheFooter.vue'
+
+export default {
+  components: {
+    TheNavbar,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+.btn:focus,
+.btn-close:focus,
+.btn-check:focus + .btn-outline-dark,
+.form-control:focus,
+.form-select:focus,
+.accordion-button:focus,
+.navbar-toggler:focus {
+  outline: 0 !important;
+  box-shadow: none !important;
+}
+
+.cursor-pointer,
+.dropdown-item {
+  cursor: pointer;
+}
+
+.cursor-default {
+  cursor: default;
+}
+
+.dropdown-item.active,
+.dropdown-item:active {
+  color: #fff;
+  text-decoration: none;
+  background-color: #ffc107;
+}
+
+.text-ellipsis {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.text-nodecoration {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/interface/TheNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-md border-bottom">
+    <div class="container-fluid pt-1">
+      <div class="d-sm-none navbar-brand p-0">
+        <img
+          src="/img/icons/logo_8.svg"
+          height="26"
+          class="d-inline-block mb-1 me-0"
+          alt="ATIOP"
+        />
+      </div>
+      <div class="d-none d-sm-block navbar-brand p-0">
+        <img
+          src="/img/icons/logo_8.svg"
+          height="26"
+          class="d-inline-block mb-1 me-2"
+          alt="ATIOP"
+        />
+        ATIOP
+      </div>
+      <div class="me-auto"></div>
+    </div>
+  </nav>
+</template>
