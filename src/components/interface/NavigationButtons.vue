f4d20bb180047d2e5ecef31c0067d205e78c7f11
mccrush <mccrush2027@gmail.com>
2023-12-27 19:59:20 +0700

    Settings work nav buttton

--- src/components/interface/NavigationButtons.vue
+++ src/components/interface/NavigationButtons.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="row lh-1">
+  <div class="row lh-1 mt-4">
     <div class="col-6 pe-1">
       <div
-        class="d-flex justify-content-start align-items-stretch rounded border bg-light cursor-pointer"
+        v-if="prevView"
+        class="d-flex justify-content-start align-items-stretch rounded border cursor-pointer nav-button"
+        @click="setPath(prevView.alias)"
       >
         <div class="d-flex align-items-center p-3">
           <div>⇐</div>
         </div>
-        <div class="p-3">Text</div>
+        <div class="p-3 ps-1">{{ prevView.title }}</div>
       </div>
     </div>
     <div class="col-6 ps-1">
       <div
-        class="d-flex justify-content-end align-items-stretch rounded border bg-light cursor-pointer"
+        v-if="nextView"
+        class="d-flex justify-content-end align-items-stretch rounded border cursor-pointer nav-button"
+        @click="setPath(nextView.alias)"
       >
-        <div class="text-end p-3">Зачем нам это надо?</div>
+        <div class="text-end p-3 pe-1">{{ nextView.title }}</div>
         <div class="d-flex align-items-center p-3">
           <div>⇒</div>
         </div>
@@ -22,3 +26,45 @@
     </div>
   </div>
 </template>
+
+<script>
+import { menuItems } from './../../data/menuItems'
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    prevView() {
+      const index = menuItems.findIndex(
+        item => item.alias === this.currentPath.slice(2)
+      )
+      return menuItems[index - 1]
+    },
+    nextView() {
+      const index = menuItems.findIndex(
+        item => item.alias === this.currentPath.slice(2)
+      )
+      return menuItems[index + 1]
+    }
+  },
+  methods: {
+    setPath(alias) {
+      window.location.hash = '#/' + alias
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
+  }
+}
+</script>
+
+<style scoped>
+.nav-button:hover {
+  background-color: #f8f9fa;
+}
+</style>
